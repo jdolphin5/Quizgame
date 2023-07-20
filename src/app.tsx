b517bee5a70549a9ca12241952d61cfc84cba45c
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-07-20 16:44:22 +1000

    working on loading mui components

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import '../styles/main.css'; // Relative path to styles/main.css
+import { Radio } from '@mui/material';
 
 const App: React.FC = () => {
   return (
     <div className = 'root'>
-      <h1>Welcome to My Quiz Game</h1>
-      {/* Your content goes here */}
+      <h1>Welcome to My Quiz GameAA</h1>
+      <h2>Question X</h2>
+      <p>What is the capital of France?</p>
+      <Radio />
+      <ul>
+        <li>Londona</li>
+        <li>Paris</li>
+        <li>Berlin</li>
+        <li>Madrid</li>
+      </ul>
     </div>
   );
 };
