a206ae3889ab5907f016716b4d89152a365c679d
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-07-20 18:57:52 +1000

    implemented score table and hook - correct radio

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,71 @@
 import React from 'react';
-import { Radio } from '@mui/material';
+import { Radio, FormLabel, RadioGroup, FormControl, FormControlLabel,
+  Table, TableBody, TableContainer, TableCell, TableHead, TableRow
+} from '@mui/material';
+import { useState } from 'react';
+
+function createData(
+  username: string,
+  score: number,
+) {
+  return { username, score };
+}
+
+const data = [
+  createData("Spongebob", 0),
+  createData("Patrick", 1),
+];
+
+
 
 const App: React.FC = () => {
+
+  const [ansCorrectCount, setAnsCorrect] = useState(0);
+
+  function handleChange() {
+    setAnsCorrect(ansCorrectCount + 1);
+  }
+
   return (
+
     <div className = 'root'>
       <h1>Welcome to My Quiz GameAA</h1>
       <h2>Question X</h2>
       <p>What is the capital of France?</p>
-      <ul>
-        <li>Londona</li>
-        <Radio />
-        <li>Paris</li>
-        <li>Berlin</li>
-        <li>Madrid</li>
-      </ul>
+
+      <FormControl>
+        <FormLabel id="questionForm">Answers:</FormLabel>
+        <RadioGroup aria-labelledby="questionLabel" name="radio-buttons-group">
+          <FormControlLabel value="London" control={<Radio />} label="London" onChange={handleChange}/>
+          <FormControlLabel value="Paris" control={<Radio />} label="Paris" />
+          <FormControlLabel value="Madrid" control={<Radio />} label="Madrid" />
+          <FormControlLabel value="New York" control={<Radio />} label="New York" />
+        </RadioGroup>
+      </FormControl>
+      <TableContainer className="ans_table_container">
+        <Table sx={{width:200}} aria-label="myTable">
+          <TableHead>
+            <TableRow sx={{border: 1, color:'black'}}>
+              <TableCell align="right">Username</TableCell>
+              <TableCell align="right">Score</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              data.map((row) => (
+                <TableRow key={row.username} sx={{border: 1, color:'gray'}}>
+                  <TableCell align="right">{row.username}</TableCell>
+                  <TableCell align="right">{row.score}</TableCell>
+                </TableRow>
+              ))
+            }
+            <TableRow sx={{border: 1, color:'gray'}}>
+            <TableCell align="right">HookUser</TableCell>
+            <TableCell align="right">{ansCorrectCount}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
