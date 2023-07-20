89c410f6e8974afba9638d9b7157d60fe5196f23
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-07-20 14:56:47 +1000

    fixed import statement

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from '../styles/main.css';
+import '../styles/main.css'; // Relative path to styles/main.css
 
 const App: React.FC = () => {
   return (
-    <div className = 'styles.root'>
+    <div className = 'root'>
       <h1>Welcome to My Quiz Game</h1>
       {/* Your content goes here */}
     </div>
