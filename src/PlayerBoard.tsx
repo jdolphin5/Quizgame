d1faacaf72728400ce211a7e7b5bbf0a4f656997
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-07-21 18:41:16 +1000

    added styling to question area

--- src/PlayerBoard.tsx
+++ src/PlayerBoard.tsx
@@ -23,19 +23,27 @@ const PlayerBoard: React.FC<PlayerBoardProps> = ({ sharedState, setSharedState }
     }
     return (
         <div>
-            <h1>Welcome to My Quiz GameAA</h1>
-            <h2>Question X</h2>
-            <p>What is the capital of France?</p>
+            <div className="header2">
+                <div className="gameName">Quiz Game</div>
+                <div className="quizName">Geography Quiz</div>
+                <div className="timer">Reading Timer: <p>10</p></div>
+            </div>
+            <div className="body2">
+                <h2>Question 1</h2>
+                <h2>What is the capital of France?</h2>
 
-            <FormControl>
-                <FormLabel id="questionForm">Answers:</FormLabel>
-                <RadioGroup aria-labelledby="questionLabel" name="radio-buttons-group">
-                <FormControlLabel value="London" control={<Radio />} label="London" onChange={handleChange}/>
-                <FormControlLabel value="1" control={<Radio />} label="Paris" onChange={handleChange}/>
-                <FormControlLabel value="Madrid" control={<Radio />} label="Madrid" onChange={handleChange}/>
-                <FormControlLabel value="New York" control={<Radio />} label="New York" onChange={handleChange}/>
-                </RadioGroup>
-            </FormControl>
+                <FormControl>
+                    <FormLabel id="questionForm"><h2>Answers:</h2></FormLabel>
+                    <RadioGroup aria-labelledby="questionLabel" name="radio-buttons-group">
+                    <FormControlLabel value="London" control={<Radio />} label="London" onChange={handleChange}/>
+                    <FormControlLabel value="1" control={<Radio />} label="Paris" onChange={handleChange}/>
+                    <FormControlLabel value="Madrid" control={<Radio />} label="Madrid" onChange={handleChange}/>
+                    <FormControlLabel value="New York" control={<Radio />} label="New York" onChange={handleChange}/>
+                    </RadioGroup>
+                </FormControl>
+                
+                <h4>Please select an answer above</h4>
+            </div>
         </div>
       );
     };
