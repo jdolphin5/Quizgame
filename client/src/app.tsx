2d86a4b6e15dbcd04a9fd50249684ccdbbe62b14
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-07-25 17:07:56 +1000

    changed component hierarchy removed axios.get

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,32 +1,15 @@
 import React from 'react';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import PlayerBoard from './PlayerBoard';
 import ControlArea from './ControlArea';
-import { QuizData } from './types';
 
 const App: React.FC = () => {
-  const [quizData, setQuizData] = useState<QuizData | null>(null);
-  const [sharedState, setSharedState] = useState(0);
-  
-  useEffect(() => {
-    const quizId = 1;
-    const numQuestions = 5;
-    axios.get(`http://localhost:3000/api/quiz/${quizId}/questions/${numQuestions}`) // /api/quiz/${quizId}/questions/${numQuestions} route
-      .then((response: any) => {
-        setQuizData(response.data); // Update the state with the fetched quiz data
-      })
-      .catch((error: any) => {
-        console.error('Error fetching quiz data:', error);
-      });
-  }, []);
+  const [userState, setUserState] = useState(0);
   
   return (
     <div className = 'root'>
-      { quizData !== null && (
-      <div className="playerBoard"><PlayerBoard sharedState={sharedState} quizData={quizData} setSharedState={setSharedState} setQuizData={setQuizData}/></div>
-      )}
-      <div className="controlArea"><ControlArea sharedState={sharedState} setSharedState={setSharedState}/></div>
+      <div className="playerBoard"><PlayerBoard userState={userState} setUserState={setUserState}/></div>
+      <div className="controlArea"><ControlArea userState={userState} setUserState={setUserState}/></div>
     </div>
   );
 };
