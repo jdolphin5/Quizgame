645a967193d26c50f535c0aa7532e92cf0e2d021
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-07-25 17:08:46 +1000

    working timer, reset, next question, added hooks

--- client/src/PlayerBoard.tsx
+++ client/src/PlayerBoard.tsx
@@ -1,13 +1,88 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Radio, FormLabel, RadioGroup, FormControl, FormControlLabel
 } from '@mui/material';
 import { QuizData, PlayerBoardProps, Answer, Question } from './types';
 import QuestionTimer from './timer';
+import axios from 'axios';
 
 let correctApplied = false;
-let shuffleAnswersNum = 0;
+const quizId = 1;
+const numQuestions = 5;
 
-const PlayerBoard: React.FC<PlayerBoardProps> = ({ sharedState, setSharedState, quizData, setQuizData }) => {
+const PlayerBoard: React.FC<PlayerBoardProps> = ({ userState, setUserState }) => {
+    const [quizData, setQuizData] = useState<QuizData | null>(null);
+    const [quizDataFetched, setQuizDataFetched] = useState(false);
+    const [questionNumber, setQuestionNumber] = useState<number>(0);
+    const [shuffledAnswers, setShuffledAnswers] = useState<Answer[]>([]);
+    const [answers, setAnswers] = useState<Answer[]>([]);
+    const [question, setQuestion] = useState<Question | null>(null);
+    const [correctAnsId, setCorrectAnsId] = useState<number>(0);
+    const [quizTitle, setQuizTitle] = useState<string>("");
+    const [timerLapsed, setTimerLapsed] = useState<boolean>(false);
+    const [shouldReset, setShouldReset] = useState<boolean>(true);
+    const [hasAnsweredQuestion, setHasAnsweredQuestion] = useState<boolean>(false);
+    const [selectedAnswer, setSelectedAnswer] = useState<number>(-1);
+
+    
+    useEffect(() => {
+        axios.get(`http://localhost:3000/api/quiz/${quizId}/questions/${numQuestions}`) // /api/quiz/${quizId}/questions/${numQuestions} route
+          .then((response: any) => {
+            setQuizData(response.data); // Update the state with the fetched quiz data
+            setQuizDataFetched(true);
+        })
+          .catch((error: any) => {
+            console.error('Error fetching quiz data:', error);
+          });
+
+      }, []);
+
+    useEffect(() => {
+        if (quizDataFetched && quizData && shuffledAnswers.length === 0) {
+            setShuffledAnswers(shuffleAnswers(answers));
+        }
+        setTimerLapsed(false);
+    }, [answers, quizDataFetched]);
+    
+
+    useEffect(() => {
+        if (quizData && questionNumber >= 0 && questionNumber < quizData.questions.length) {
+          let currentQuestion = quizData.questions[questionNumber];
+          let answers = quizData.answers.filter(ans => ans.question_id === currentQuestion.question_id);
+          let correctAnsId: number = 0;
+          answers.forEach((answer) => {
+            if (answer.is_correct) {
+              correctAnsId = answer.answer_id;
+            }
+          });
+
+        if (questionNumber >= numQuestions-1) {
+            setShouldReset(false);
+        }
+          
+        setHasAnsweredQuestion(false);
+        setQuestion(currentQuestion);
+        setAnswers(answers);
+        setCorrectAnsId(correctAnsId);
+        setQuizTitle(quizData.quiz.title);
+        setShuffledAnswers([]);
+        }
+      }, [questionNumber, quizData]);
+
+    useEffect(() => {
+        if (timerLapsed) {
+            if (questionNumber < numQuestions-1) { //increment from 0 to numQuestions-1
+                setQuestionNumber(questionNumber+1);
+            }
+            if (!hasAnsweredQuestion) {
+                setHasAnsweredQuestion(true);
+                console.log(selectedAnswer);
+                console.log(correctAnsId);
+                if (selectedAnswer === correctAnsId) {
+                    setUserState(userState + 1);
+                }
+            }
+        }
+    }, [timerLapsed])
 
     if (!quizData) {
         return <div>Loading...</div>;
@@ -15,12 +90,7 @@ const PlayerBoard: React.FC<PlayerBoardProps> = ({ sharedState, setSharedState, 
 
     console.log(quizData);
 
-    const quizTitle = quizData.quiz.title;
-    const firstQuestion = quizData.questions[0];
-    const answers = quizData.answers.filter(ans => ans.question_id === firstQuestion.question_id);
-
-    const [shuffledAnswers, setShuffledAnswers] = useState<Answer[]>([]);
-
+    
     const shuffleAnswers = (array: Answer[]) => { 
         for (let i = array.length - 1; i > 0; i--) { 
             const j = Math.floor(Math.random() * (i + 1)); 
@@ -29,47 +99,30 @@ const PlayerBoard: React.FC<PlayerBoardProps> = ({ sharedState, setSharedState, 
         return array;
     };
 
-    useEffect(() => {
-        const shuffled = shuffleAnswers(answers);
-        setShuffledAnswers(shuffled);
-    }, [shuffleAnswersNum]);
-
-    let correctAnsId: number = 0;
-    answers.forEach((answer) => {
-        if (answer.is_correct) {
-            correctAnsId = answer.answer_id;
-        }
-    });
     console.log(correctAnsId);
-
-    let questionNumber = 1;
 
     let selectedAns: number = -1;
 
     const handleChange = (event: any) => {
-      selectedAns = +event.target.value; //convert value of the selectedAnswer from string to number
-      if (selectedAns === correctAnsId && !correctApplied) {
-        setSharedState(sharedState + 1);
-        correctApplied = true;
-      }
+      setSelectedAnswer(+event.target.value); //convert value of the selectedAnswer from string to number
     }
 
     const handleTimeUp = () => {
-
+        setTimerLapsed(true);
     }
 
     return (
         <div>
             <div className="header2">
                 <div className="gameName">
-                    {quizTitle}
+                    Quiz Game
                 </div>
-                <div className="quizName">Geography Quiz</div>
-                <div className="timer">Timer: <p>{<QuestionTimer timeLimit={10} onTimeUp={handleTimeUp} />}</p></div>
+                <div className="quizName">{quizTitle}</div>
+                <div className="timer">Timer: <p>{<QuestionTimer shouldReset={shouldReset} timeLimit={10} onTimeUp={handleTimeUp} />}</p></div>
             </div>
             <div className="body2">
-                <h2>Question {questionNumber}</h2>
-                <h2>{firstQuestion.question_text}</h2>
+                <h2>Question {questionNumber+1}</h2>
+                <h2>{question?.question_text}</h2>
 
                 <FormControl>
                     <FormLabel id="questionForm"><h2>Answers:</h2></FormLabel>
