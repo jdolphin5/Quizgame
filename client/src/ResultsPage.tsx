d8763ad4a5a9eb34d3987730aaeeb85037a98bb4
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-08-04 16:02:04 +1000

    add legend and green for correct red for selected

--- client/src/ResultsPage.tsx
+++ client/src/ResultsPage.tsx
@@ -12,57 +12,62 @@ const ResultsPage: React.FC<ResultPageProps> = ({ userResults, userState, showMo
 
     return (
         <Modal
-            style={{overflow: "auto"}}
+            className="results"
             open={showModal}
             onClose={handleCloseModal}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
             <Paper>
-                <h2 id="modal-modal-title">{userState[0].username}'s Quiz Results</h2>
-                <Table>
-                    <TableHead>
-                        <TableRow sx={{border: 1, color:'black'}}>
-                            <TableCell sx={{}} align="left">Question</TableCell>
-                            <TableCell sx={{}} align="left">Answer</TableCell>
-                            <TableCell sx={{}} align="left">Selected</TableCell>
-                            <TableCell sx={{}} align="left">Correct</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            userResults.map((result: Result, i) => (
-                                <React.Fragment key={i}>
-                                    <TableRow>
-                                        <TableCell>
-                                            {result.question.question_text}
-                                        </TableCell>
-                                    </TableRow>
-                                    {result.answers.map((answer: Answer, j) => (
-                                        <React.Fragment key={j}>
-                                            <TableRow>
-                                                <TableCell>
-                                                </TableCell>
-                                                <TableCell>
-                                                    {answer.answer_text}
-                                                </TableCell>
-                                                <TableCell>
-                                                    {String(answer.is_correct)}
-                                                </TableCell>
-                                                <TableCell>
-                                                    {String(answer.answer_id === result.selected_answer_id)}
-                                                </TableCell>
-                                            </TableRow>
-                                        </React.Fragment>
-                                    ))}
-                                </React.Fragment>
-                            ))
-                        }
-                    </TableBody>
-                </Table>
-                <Button variant="contained" color="primary" onClick={handleCloseModal}>
-                    Close Results
-                </Button>
+                <div className="resultsPaper">
+                    <h2>{userState[0].username}'s Quiz Results</h2>
+                    <div className="legend-container">
+                    <div className="legend-box correct-box"></div>
+                    Correct
+                    <br />
+                    <div className="legend-box selected-box"></div>
+                    Selected
+                    </div>
+                    <br />
+                    <Table>
+                        <TableHead>
+                            <TableRow sx={{border: 1, color:'black'}}>
+                                <TableCell sx={{}} align="left">Question</TableCell>
+                                <TableCell sx={{}} align="left">Answer</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                userResults.map((result: Result, i) => (
+                                    <React.Fragment key={i}>
+                                        <TableRow>
+                                            <TableCell>
+                                                {result.question.question_text}
+                                            </TableCell>
+                                        </TableRow>
+                                        {result.answers.map((answer: Answer, j) => (
+                                            <React.Fragment key={j}>
+                                                <TableRow>
+                                                    <TableCell>
+                                                    </TableCell>
+                                                    <TableCell sx={{border: answer.answer_id === result.selected_answer_id ? '1px solid #000000' : '0',}}
+                                                    className={
+                                                        answer.is_correct ? "table-correct" : answer.answer_id === result.selected_answer_id ? "table-selected" : "table-incorrect"
+                                                    }>
+                                                        {answer.answer_text}
+                                                    </TableCell>
+                                                </TableRow>
+                                            </React.Fragment>
+                                        ))}
+                                    </React.Fragment>
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+                    <Button variant="contained" color="primary" onClick={handleCloseModal}>
+                        Close Results
+                    </Button>
+                </div>
             </Paper>
         </Modal>
     );
