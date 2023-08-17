1a0b39db910404054ed38d900d4b2b16bcce47bd
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-08-17 14:03:21 +1000

    tests added for setInterval and clearInterval

--- client/src/__tests__/QuestionTimer.test.tsx
+++ client/src/__tests__/QuestionTimer.test.tsx
@@ -1,18 +1,79 @@
 import React from 'react';
-import { render, screen, cleanup } from '@testing-library/react';
-// Importing the jest testing library
+import { render, screen, cleanup, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import QuestionTimer from '../QuestionTimer';
 
-// afterEach function runs after each test suite is executed
+beforeEach(() => {
+    jest.useFakeTimers();
+});
+
 afterEach(() => {
+    jest.clearAllTimers();
     cleanup(); // Resets the DOM after each test suite
 });
 
-describe('Timer Component', () => {
-    const shouldReset: boolean = true;
+test('Timer starts counting down', () => {
+    const shouldReset: boolean = false;
     const timerValue: number = 10;
     const handleTimeUp: any = jest.fn();
+
+    const setIntervalSpy = jest.spyOn(global, 'setInterval');
+
+    act(() => {
+        render(
+            <QuestionTimer
+                shouldReset={shouldReset}
+                timeLimit={timerValue}
+                onTimeUp={handleTimeUp}
+            />
+        );
+    });
+
+    expect(setIntervalSpy).toHaveBeenCalledTimes(1);
+    expect(setIntervalSpy).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+
+    act(() => {
+        jest.advanceTimersByTime(1000);
+    });
+
+    expect(handleTimeUp).not.toHaveBeenCalled();
+
+    setIntervalSpy.mockRestore();
+});
+
+test('Timer finishes and calls clearInterval', () => {
+    const shouldReset: boolean = false;
+    const timerValue: number = 10;
+    const handleTimeUp: any = jest.fn();
+
+    const clearIntervalSpy = jest.spyOn(global, 'clearInterval');
+
+    act(() => {
+        render(
+            <QuestionTimer
+                shouldReset={shouldReset}
+                timeLimit={timerValue}
+                onTimeUp={handleTimeUp}
+            />
+        );
+    });
+
+    act(() => {
+        jest.advanceTimersByTime(timerValue * 1000);
+    });
+
+    expect(handleTimeUp).toHaveBeenCalledTimes(1);
+    expect(clearIntervalSpy).toHaveBeenCalledTimes(1);
+    expect(clearIntervalSpy).toHaveBeenLastCalledWith(expect.any(Number));
+
+    clearIntervalSpy.mockRestore();
+});
+
+test('Timer renders correctly', () => {
+    const shouldReset: boolean = true;
+    const timerValue: number = 10;
+    const handleTimeUp = jest.fn();
+
     render(
         <QuestionTimer
             shouldReset={shouldReset}
@@ -20,15 +81,9 @@ describe('Timer Component', () => {
             onTimeUp={handleTimeUp}
         />
     );
-    const timer = screen.getByTestId('timer');
 
-    // Test 1
-    test('Timer Rendering', () => {
-        expect(timer).toBeInTheDocument();
-    });
+    const timer = screen.getByTestId('question-timer');
 
-    // Test 2
-    test('Timer Has Text Content', () => {
-        expect(timer).toHaveTextContent('1');
-    });
+    expect(timer).toBeInTheDocument();
+    expect(timer).toHaveTextContent('10');
 });
